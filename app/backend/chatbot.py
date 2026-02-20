c6ff500f4d6656b1a47031011c303d18269dce52
Philipp Bergsmann <pbergsma@redhat.com>
2026-02-20 11:48:23 +0100

    fix: log response result in Chatbot for improved debugging

--- app/backend/chatbot.py
+++ app/backend/chatbot.py
@@ -241,9 +241,10 @@ class Chatbot:
             self.logger.info(f"Claim: {claim}")
             self.logger.info(f"Response: {resp}")
             self.logger.info(f"Response Keys: {resp.keys()}")
+            self.logger.info(f"Response Result: {resp['result']}")
             self.logger.info(f"Sources: {resp['source_documents']}")
             if os.getenv("AI_SERVICE") == "aws": # AWS Bedrock Knowledge Bases does not return source documents in the same way as the other vector DB based retrievers
-                data = {"type": "token", "token": resp['content']}
+                data = {"type": "token", "token": resp['result']}
                 self.q.put(data)
             else:
                 sources = self.format_sources(resp['source_documents'])
